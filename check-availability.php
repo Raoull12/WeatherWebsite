88c861dc2e61c6b74f3706b7414e424df3388939
Raoull12 <raoulborg02@gmail.com>
2023-12-16 23:19:50 +0100

    final final

--- check-availability.php
+++ check-availability.php
@@ -6,41 +6,57 @@ if (!empty($_POST["email"])) {
     $email = $_POST["email"];
     $userId = isset($_SESSION["id"]) ? $_SESSION["id"] : null;
 
-    // Check if the email already exists for other users (excluding the current user)
-    $sql = "SELECT COUNT(*) as count FROM users WHERE email = ? AND id != ?";
-    $query = $mysqli->prepare($sql);
-    $query->bind_param("si", $email, $userId);
-    $query->execute();
-    $query->bind_result($count);
-    $query->fetch();
+    if ($userId !== null) {
+        // Editing profile: Check if the email already exists for other users (excluding the current user)
+        $sql = "SELECT COUNT(*) as count FROM users WHERE email = ? AND id != ?";
+        $query = $mysqli->prepare($sql);
+        $query->bind_param("si", $email, $userId);
+        $query->execute();
+        $query->bind_result($count);
+        $query->fetch();
+    } else {
+        // Registering: Check if the email already exists for any user
+        $sql = "SELECT COUNT(*) as count FROM users WHERE email = ?";
+        $query = $mysqli->prepare($sql);
+        $query->bind_param("s", $email);
+        $query->execute();
+        $query->bind_result($count);
+        $query->fetch();
+    }
 
     if ($count > 0) {
         echo "<span style='color: red;'>Email Already Exists.</span>";
-        echo "<script>$('#submit').prop('disabled', true);</script>"; //disabling the button if an invalid email is inputted
     } else {
-        echo "<span style='color: green;'>Email Available.</span>"; 
-        echo "<script>$('#submit').prop('disabled', false);</script>"; //enabling the button if an invalid username is inputted
+        echo "<span style='color: green;'>Email Available.</span>";
     }
 }
 
 if (!empty($_POST["username"])) {
-  $username = $_POST["username"];
-  $userId = isset($_SESSION["id"]) ? $_SESSION["id"] : null;
+    $username = $_POST["username"];
+    $userId = isset($_SESSION["id"]) ? $_SESSION["id"] : null;
 
-  // Check if the username already exists for other users (excluding the current user)
-  $sql = "SELECT COUNT(*) as count FROM users WHERE username = ? AND id != ?";
-  $query = $mysqli->prepare($sql);
-  $query->bind_param("si", $username, $userId);
-  $query->execute();
-  $query->bind_result($count);
-  $query->fetch();
+    if ($userId !== null) {
+        // Editing profile: Check if the username already exists for other users (excluding the current user)
+        $sql = "SELECT COUNT(*) as count FROM users WHERE username = ? AND id != ?";
+        $query = $mysqli->prepare($sql);
+        $query->bind_param("si", $username, $userId);
+        $query->execute();
+        $query->bind_result($count);
+        $query->fetch();
+    } else {
+        // Registering: Check if the username already exists for any user
+        $sql = "SELECT COUNT(*) as count FROM users WHERE username = ?";
+        $query = $mysqli->prepare($sql);
+        $query->bind_param("s", $username);
+        $query->execute();
+        $query->bind_result($count);
+        $query->fetch();
+    }
 
-  if ($count > 0) {
-      echo "<span style='color: red;'>Username Already Exists.</span>"; //disabling the button if an invalid username is inputted
-      echo "<script>$('#submit').prop('disabled', true);</script>";
-  } else {
-      echo "<span style='color: green;'>Username is available.</span>";
-      echo "<script>$('#submit').prop('disabled', false);</script>"; //enabling the button if an invalid username is inputted
-  }
+    if ($count > 0) {
+        echo "<span style='color: red;'>Username Already Exists.</span>";
+    } else {
+        echo "<span style='color: green;'>Username is available.</span>";
+    }
 }
 ?>
